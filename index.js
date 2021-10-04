9d0bf6fcb054dd55b5518a00ae126ad16f39e6d8
nir.jacob <nir.jacob@is.com>
2021-10-04 17:06:31 +0300

    branch test!

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 const express = require('express')
 const app = express()
 const port = 3000
-var cowsay = require("cowsay");
-
-console.log(cowsay  .say({
-    text : "Hello World",
-    e : "^^",
-    T : "U "
-}));
