163dc784c647e987a28a7afca69ae27a81fa3749
nir.jacob <nir.jacob@is.com>
2021-10-05 17:51:04 +0300

    Exercise #2 Complete (Pre PR)

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.port || 3000
+
+// File System
 const fs = require('fs');
+
+//Body Parser
 const bodyParser= require('body-parser')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
 // Data layer
 const FILE_PATH = './DataLayer/podcasts.json'
-const podcastData = require(FILE_PATH)
+let podcastData = require(FILE_PATH)
 
 //Validator
 const joi = require('joi')
@@ -19,42 +23,63 @@ app.get('/podcast/:id',(req,res)=>{
     if(error) return res.status(400).send(error.details[0].message)
 try{
     const podcastInfo = getPodcastById(queryParams.id)
-    return res.send(podcastInfo)
+    return res.status(200).send(podcastInfo)
     }
 catch(err){
     console.log(`Error: ${err.stack}`)
-    res(500).send(`Server Error: ${err.message}`)
+    res.status(500).send(`Server Error: ${err.message}`)
 }
 })
 
 app.post('/podcast/new',(req,res)=>{
-    console.log()
-    if(getPodcastById(req.body.id) == null){
-        savePodcast(req.body)
-        return res(200).send("Podcast has been successfully added")
+    if(getPodcastById(req.body.id).length === 0){
+         try{
+             savePodcast(req.body)
+         }
+         catch(err){
+             console.log(`Error: ${err.stack}`)
+             res.status(500).send(`Server file error: ${err.message}`)
+         }
+        return res.status(200).send("Podcast has been successfully added")
     }
     else{
-        return res(409).send("Unable to add, podcast already exists!")
+        return res.status(409).send("Unable to add, podcast already exists!")
     }
 })
 
-app.delete('/podcast/:id',(res,req)=>{
+app.delete('/podcast/:id',(req,res)=>{
     const queryParams = req.params
     const {error} = ValidateInput(queryParams)
-    if(error) return res(400).send(error.details[0].message)
+    if(error) return res.status(400).send(error.details[0].message)
     try{
         const podcastInfo = getPodcastById(queryParams.id)
         if(podcastInfo.length !== 0){//none empty array
             deletePodcast(queryParams.id)
-            return res.delete(podcastData[id])
+            return res.status(200).send('Podcast has been successfully removed')
         }
         else{
-            res(404).send('Podcast not found')
+            res.status(404).send('Podcast not found')
         }
     }
     catch(err){
         console.log(`Error: ${err.stack}`)
-        res(500).send(`Server Error: ${err.message}`)
+        res.status(500).send(`Server Error: ${err.message}`)
+    }
+})
+
+app.put('/podcast/:id',(req,res)=> {
+    if(getPodcastById(req.params.id).length !== 0){
+        try{
+            updatePodcasts(req.body,req.params.id)
+        }
+        catch(err){
+            console.log(`Error: ${err.stack}`)
+            res.status(500).send(`Server file error: ${err.message}`)
+        }
+        return res.status(200).send("Podcast has been successfully updated!")
+    }
+    else{
+        return res.status(409).send("Unable to update, podcast does not exists!")
     }
 })
 
@@ -70,28 +95,21 @@ function ValidateInput(queryParams){
 
 function getPodcastById (id) {
     return podcastData.filter(getPodcast=>getPodcast.id == id)
-    // for(let i in podcastData) {
-    //     // i is the index of each object within json data
-    //     if(podcastData[i].id == id) {
-    //         return podcastData[i]
-    //     }
-    // }
-    // return null
 }
 
-
-
 function deletePodcast(id){
-    delete podcastData.filter(getPodcast=>getPodcast.id == id)
-    //
-    // for(let i in podcastData) {
-    //     // i is the index of each object within json data
-    //     if(podcastData[i].id == id) {
-    //         delete podcastData[i]
-    //     }
-    // }
+    podcastData = podcastData.filter(getPodcast=>getPodcast.id != id)
+    fs.writeFileSync(FILE_PATH, JSON.stringify(podcastData), { flag: 'w+' })
 }
 
 function savePodcast(podcast){
-    fs.writeFileSync(FILE_PATH, JSON.stringify(podcast), { flag: 'w+' })
+    podcastData.push(podcast)
+    fs.writeFileSync(FILE_PATH, JSON.stringify(podcastData), { flag: 'w+' })
+}
+
+function updatePodcasts(updateDetails,id){
+    let podcastToUpdate = podcastData.filter(getPodcast=>getPodcast.id == id)
+    podcastToUpdate = {...podcastToUpdate[0],...updateDetails} // assign's new properties and updates existing one's
+    podcastData = podcastData.filter(getPodcast=>getPodcast.id != id)
+    savePodcast(podcastToUpdate)
 }
