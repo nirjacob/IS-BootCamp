456077cc55dfb98951bf2192bf5fc404bcc8d2ce
nir.jacob <nir.jacob@is.com>
2021-10-18 13:47:13 +0300

    fixed latest cache comments, moved to json format.

--- middleware/cacheHandler.js
+++ middleware/cacheHandler.js
@@ -1,57 +1,39 @@
 const config = require('config')
-const urlGetter = require('url')
 
-const getUrl = (req) => {
-  return urlGetter.format({
-    protocol: req.protocol,
-    host: req.get('host'),
-    pathname: req.originalUrl
-  })
+const formatCacheData = (requestUrl, timeStamp, data) => {
+  return JSON.stringify({ url: requestUrl, timeStamp: timeStamp, data: data })
 }
-
-class CachedData {
-  constructor (requestUrl, timeStamp, data) {
-    this.url = requestUrl
-    this.timeStamp = timeStamp
-    this.data = data
-  }
-
-  getData () {
-    return this.data
-  }
-
-  isExpired () {
-    return this.timeStamp + cacheMillisecondsToLive < Date.now()
-  }
+const isExpired = (timeStamp) => {
+  return timeStamp + cacheMillisecondsToLive < Date.now()
 }
 
 const cacheDataMap = new Map()
-const cacheMinutesToLive = 10
+const cacheMinutesToLive = 0.1
 const cacheMillisecondsToLive = cacheMinutesToLive * 60 * 1000
 
 const saveToCache = (url, data) => {
-  cacheDataMap.set(url, new CachedData(url, Date.now(), data))
+  cacheDataMap.set(url, formatCacheData(url, Date.now(), data))
 }
 
 const isInCache = (requestedUrl) => {
   if (requestedUrl) {
-    return !requestedUrl.isExpired()
+    return !isExpired(requestedUrl.timeStamp)
   }
   return null
 }
+
 const handleCachedData = (req, res, next) => {
-  if (config.isCacheEnabled) {
-    const requestedUrl = cacheDataMap.get(getUrl(req))
+  if (req.method === 'GET' && config.isCacheEnabled) {
+    const url = req.url
+    const requestedUrl = cacheDataMap.get(url)
     if (isInCache(requestedUrl)) {
-      return res.status(200).send(requestedUrl.getData())
+      return res.status(200).send(JSON.parse(requestedUrl).data)
     } else {
-      if (req.method === 'GET') {
-        const returnedResponse = res.send
-        res.send = (body) => {
-          saveToCache(getUrl(req), body)
-          res.send = returnedResponse
-          res.send(body)
-        }
+      const returnedResponse = res.send
+      res.send = (body) => {
+        saveToCache(url, body)
+        res.send = returnedResponse
+        res.send(body)
       }
     }
   }
