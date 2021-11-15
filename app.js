910c033911dbb395972066ae6017208f90102255
nir.jacob <nir.jacob@is.com>
2021-11-15 14:07:06 +0200

    * finished front page.

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const router = require('./routes/index')
 const bodyParser = require('body-parser')
 const { handleCachedData } = require('./middleware/cacheHandler')
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(handleCachedData)
 app.use(router)
 
